8a6b99a421e7af283e7e82952d4580c938cd93d8
Matthew <mpool@umass.edu>
2018-02-14 13:09:28 -0500

    requirements

--- sgnl_proc.py
+++ sgnl_proc.py
@@ -1,18 +1,23 @@
 import glob
 import librosa
-from scipy.signal import buttord, butter, lfilter
+from scipy.signal import butter, filtfilt
 import matplotlib.pyplot as plt
+import sounddevice as sd
+import numpy as np
 
-N, Wn = buttord([2*1000/44100, 2*12000/44100], [2*800/44100, 2*13000/44100], 1, 30)
+fsM = 44100
+Wn = [2*800/fsM, 2*12800/fsM]
+N = 10
 b, a = butter(N, Wn, 'band')
-fsM=44100
-
-for filename in glob.glob("C:/Users/Matthew/Documents/Matthew's Files/College Course Work/Senior Year/Senior Design Project/Bird Songs/*.wav"):
-    data, fs = librosa.load(filename, 44100, True)
-    print(fs)
-    vocal = lfilter(b, a, data)
-    print(data)
-    print(vocal)
-    plt.plot(data)
+for filename in glob.glob("C:/Users/Matthew/Documents/Matthew's Files/College Course Work/Senior Year/Senior Design Project/Bird Songs/*.mp3"):
+    data, fs = librosa.load(filename, fsM, True)
+    vocal = filtfilt(b, a, data)
+    intervals = librosa.effects.split(vocal, top_db=10, frame_length=1024, hop_length=512)
+    sounds = np.array([])
+    for i in range(np.ma.size(intervals, 0)):
+        sounds1 = vocal[intervals[i, 0]:intervals[i, 1]:1]
+        sounds = np.concatenate((sounds, sounds1))
     plt.plot(vocal)
+    plt.plot(sounds)
+    sd.play(sounds, fsM)
     plt.show()
